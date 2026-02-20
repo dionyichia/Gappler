1f56c140d8b3dd465e6ffc627402ca3510411e1b
Axemortal <wuyifan2001@gmail.com>
2026-02-20 09:05:01 +0800

    Remove external repositories

--- external_repositories/robot_pkg/src/yolo_segmentation/test.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('trained_model.pt')
-
-results = model('path/to/image.jpg')    
-annotated_frame = results[0].plot()
--- external_repositories/robot_pkg/src/yolo_segmentation/train.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8x-seg.pt')
-model.train(data='Dataset10.0/data.yaml', epochs=100, imgsz=(848,480), batch=0.95)
