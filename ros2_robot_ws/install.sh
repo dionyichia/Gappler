0d1570a4e46ae27963ed56410a7d0501aa9ddd46
axmortal <wuyifan2001@gmail.com>
2026-03-10 15:47:33 +0800

    Added MTC as a dependency

--- ros2_robot_ws/install.sh
+++ ros2_robot_ws/install.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [ ! -d "$HOME/deps_ws/install" ]; then
+    echo "Building dependencies in ~/deps_ws..."
+    (cd "$HOME/deps_ws" && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release)
+fi
+
+# Source the dependency workspace so this build can find MTC
+source "$HOME/deps_ws/install/setup.bash"
+
 echo "Cleaning workspace..."
 rm -rf ./logs ./build ./install
 
@@ -12,6 +20,8 @@ echo "Sourcing install/setup.bash..."
 source ./install/setup.bash
 
 echo "Building all packages..."
-colcon build
+colcon build --symlink-install
+
+
 
 echo "Done!"
