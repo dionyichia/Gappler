7e5f4efb206d1b651f47d8ccccb1760c87f2b50b
Axemortal <wuyifan2001@gmail.com>
2026-04-10 02:05:41 +0800

    Fix merge conflict errors

--- src/main.py
+++ src/main.py
@@ -18,13 +18,8 @@ from schemas.application import ApplicationConfig
 from services.process_manager import ProcessManager
 from utils import TerminalRawMode, exit_keypress, safe_update_iptables, setup_logging
 
-os.environ["QT_QPA_FONTDIR"] = "/usr/share/fonts"  # Point to system fonts
-os.environ["QT_QUICK_BACKEND"] = "software"
+# Logging setup
 setup_logging()
-
-SCRIPT_DIR = Path(__file__).resolve().parent
-PROJECT_ROOT = SCRIPT_DIR.parent
-
 logger = logging.getLogger(__name__)
 
 if not rclpy.ok():
@@ -385,13 +380,15 @@ def parse_arguments() -> ApplicationConfig:
         "--mode",
         choices=["live", "recording"],
         default="live",
-        help="Mode to run the package in (default: %(default)s)",
+        help="Mode to run the application in (default: %(default)s)",
     )
+
     parser.add_argument(
         "--recording-path",
         type=str,
         help="Path to recording folder (required when mode is 'recording')",
     )
+
     parser.add_argument(
         "--track-pose",
         action="store_true",
@@ -405,8 +402,13 @@ def parse_arguments() -> ApplicationConfig:
         help="Update iptables to enable receiving the data stream (Linux only)",
     )
 
+    parser.add_argument("--device_ip", type=str, help="IP Address of Aria Glasses")
+
+    parser.add_argument("--profile_name", type=str, help="Profile used for streaming")
+
     args = parser.parse_args()
-    # Validate that recording-path is provided when mode is 'recording'
+
+    # Validate recording mode requirements
     if args.mode == "recording" and not args.recording_path:
         parser.error("--recording-path is required when --mode is 'recording'")
 
